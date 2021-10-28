1ab16b3061e52f5ebb8d4e7e33c51ff455db262d
stevenrdz <stevenrodriguezm97@gmail.com>
2021-10-28 17:26:47 -0500

    Error RouterLinkActive 28/10

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,11 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SidebarComponent implements OnInit {
+
+export class SidebarComponent{
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
